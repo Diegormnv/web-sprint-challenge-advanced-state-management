e3a37466083c257ea80b3bc82580747a347a1d02
Diego Roman <diegormnv209@gmail.com>
2021-01-22 10:46:22 -0800

    Smurf list is dislpaying. Need to clean up

--- src/components/Smurf.js
+++ src/components/Smurf.js
@@ -5,6 +5,11 @@ class Smurf extends React.Component {
         const { smurf } = this.props;
 
         return(<div data-testid="smurf" className="card">
+        <p>{smurf.name}</p>;
+        <p>{smurf.position}</p>;
+        <p>{smurf.nickname}</p>;
+        <p>{smurf.description}</p>;
+
         </div>);
     }
 }
--- src/components/SmurfDisplay.js
+++ src/components/SmurfDisplay.js
@@ -1,14 +1,42 @@
 import React from 'react';
+import { getSmurf } from '../actions';
+import { connect } from 'react-redux';
+
+import Smurf from './Smurf';
 
 export class SmurfDisplay extends React.Component {
+    
+
+    componentDidMount() {
+        this.props.getSmurf();
+    }
+
     render() {
+        console.log('Hellooooo', this.props);
+        if(this.props.isFetching){
+            return <h3>Fetching Smurfs...</h3>
+        }
+
+        if(this.props.error){
+            return <h3>OOPS! An error occured rerieving Smurfs!</h3>
+        }
         return(<div>
-            
+            {this.props.smurf.map((character) =>{
+                return <Smurf smurf={character} />
+            })}
         </div>)
     }
 }
 
-export default SmurfDisplay;
+const mapStateToProps = state =>{
+    return{
+        smurf: state.smurf,
+        isFetching: state.isFetching,
+        error: state.error
+    }
+}
+
+export default connect(mapStateToProps, {getSmurf}) (SmurfDisplay);
 
 //Task List:
 //1. Import in all needed components and library methods.
